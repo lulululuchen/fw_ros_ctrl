f02d07c7f085383a1359d6ce12846050ee5b5f77
Thomas Stastny <thomas.stastny@mavt.ethz.ch>
2019-12-16 14:41:03 +0100

    correct attitude setpoint msg

--- src/fw_nmpc.cpp
+++ src/fw_nmpc.cpp
@@ -108,7 +108,7 @@ FwNMPC::FwNMPC() :
     wind_est_sub_ = nmpc_.subscribe("/mavros/wind_estimation", 1, &FwNMPC::windEstCb, this);
 
     /* publishers */
-    att_sp_pub_ = nmpc_.advertise<geometry_msgs::PoseStamped>("/mavros/setpoint_attitude/target_attitude", 1);
+    att_sp_pub_ = nmpc_.advertise<geometry_msgs::PoseStamped>("/mavros/setpoint_attitude/attitude", 1);
     nmpc_info_pub_ = nmpc_.advertise<fw_ctrl::NMPCInfo>("/nmpc/info",10);
     nmpc_meas_pub_ = nmpc_.advertise<fw_ctrl::NMPCMeasurements>("/nmpc/measurements",10);
     nmpc_states_pub_ = nmpc_.advertise<fw_ctrl::NMPCStates>("/nmpc/states",10);
@@ -452,7 +452,7 @@ void FwNMPC::publishControls(const double u_T, const double phi_ref, const doubl
 
     // attitude setpoint
     geometry_msgs::PoseStamped att_sp;
-    att_sp.header.frame_id = "world";
+    att_sp.header.frame_id = "map";
     att_sp.header.stamp = ros::Time::now();
     tf::Quaternion q_tf;
     q_tf.setRPY(phi_ref, theta_ref, 0.0);
